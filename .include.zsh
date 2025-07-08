787a541e6297016dae176647b0e1fef7428d2c77
Mathias T. Gleerup <mathias@aleta.io>
2025-07-08 09:35:54 +0200

    update to current state

---
+++ .include.zsh
@@ -0,0 +1,12 @@
+# source this script from .zshrc
+
+# options
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagaced
+export TERM=xterm-256color
+export EDITOR=nvim
+
+
+# aliases
+alias ls='ls -a'
+alias vim='nvim'
